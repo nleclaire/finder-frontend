6b43d867d1fad45e3cf82c28af95729154bed13c
nleclaire <noah.leclaire@gmail.com>
2021-05-17 16:38:19 -0400

    Carousel interval working

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -12,14 +12,21 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    // init materialize carousel
     document.addEventListener('DOMContentLoaded', () => {
       M.AutoInit();
       const options = {
         fullWidth: true,
-        indicators: true
+        indicators: true,
+        duration: 400,
+        padding: 20,
       };
       const elem = document.querySelector('.carousel');
       const instance = M.Carousel.init(elem, options);
+      // console.log(M);
+      setInterval(() => {
+        instance.next();
+      }, 5000);
     });
 
   }
