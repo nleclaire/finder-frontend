379a65ed7bb058052924b92ed0e2302c0840c61f
nleclaire <noah.leclaire@gmail.com>
2021-05-18 13:16:37 -0400

    Initializing authorization component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,17 @@ import { HomeComponent } from './components/home/home.component';
 import { SearchbarComponent } from './components/searchbar/searchbar.component';
 import {FormsModule} from '@angular/forms';
 import {HttpClientModule} from '@angular/common/http';
+import { LoginComponent } from './components/auth/login/login.component';
+import { SignupComponent } from './components/auth/signup/signup.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     HomeComponent,
-    SearchbarComponent
+    SearchbarComponent,
+    LoginComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
