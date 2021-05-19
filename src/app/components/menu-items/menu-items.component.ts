2d85902542faf483fd8538783dc18f05f1d76ab0
Shaheer Ahmed <sahmed3721@gmail.com>
2021-05-19 15:12:10 -0500

    added restaurant and menuItem service, able to retrieve restaurants and menuitems of particular place

--- src/app/components/menu-items/menu-items.component.ts
+++ src/app/components/menu-items/menu-items.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {MenuItemService} from '../../services/menuItem/menu-item.service';
 
 @Component({
   selector: 'app-menu-items',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MenuItemsComponent implements OnInit {
 
-  constructor() { }
+  menuItems: [];
+
+  constructor(private menuItemService: MenuItemService) { }
 
   ngOnInit(): void {
+    this.menuItemService.getMenuItems$().subscribe(response => {
+      this.menuItems = response;
+      console.log(this.menuItems);
+    });
   }
 
 }
